96ec389cfc965a03d34304f7d118520266936b12
samokello <samokello024@gmail.com>
2021-12-14 16:42:52 +0300

    first commit

---
+++ pages/Main/Main.js
@@ -0,0 +1,35 @@
+import styles  from "./Main.module.css";
+import Image from "next/image";
+import sam10 from "../../public/Images/sam10.png"
+
+const Main = () => {
+    return ( 
+
+
+        <div className={styles.mainpage}>
+<div className={styles.intro}>
+    <h1>WELCOME TO MY PORTFOLIO</h1>
+<p>The purpose of javaScript Mastery is to aspiring and established developers to take theire development skills to the next level and build awesome websites </p>
+<button className={styles.btn}>Learn More</button>
+</div>
+
+
+<div className={styles.introduction}>
+<div className={styles.page}>
+<Image src={sam10} width={""} height={""}/>
+
+</div>
+<div className={styles.pagecontent}>
+
+<h2>HELLO!!My name is:</h2>
+<h2>SAM OKELLO,Front-end developer.Do you live and breath digital development?? I'm your Developer.</h2>
+</div>
+</div>
+
+       </div>
+
+
+    );
+}
+ 
+export default Main;
---
+++ pages/Nav/Nav.js
@@ -0,0 +1,58 @@
+import styles from "./Nav.module.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
+import ClearIcon from '@mui/icons-material/Clear';
+import Image from "next/image";
+
+const Nav = () => {
+    const [menu, setMenu] = useState(true)
+const hundleManuClick=()=>{
+setMenu(!menu);
+
+}
+
+const renderMenu=()=>{
+    return(
+
+        <div className={styles.nav}>
+            <div className={styles.backgroundimage}>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">Colaboration</a></li>
+                <li><a href="#">Skills</a></li>
+               
+
+            </ul>
+            </div>  
+        </div>
+       
+    )
+}
+
+    return ( 
+
+        <div className={styles.content}>
+            <div className={styles.logodisplay}>
+            <div className={styles.mylogo}>
+
+            </div>
+            <div className={styles.logo}>
+                <h1>PORTFOLIO</h1>
+            </div>
+            </div>
+<div className={styles.icons} onClick={hundleManuClick}>
+    {menu?<MenuIcon />:<ClearIcon />}
+</div>
+
+
+    {menu===false?renderMenu():<></>}
+
+
+        </div>
+     );
+}
+ 
+export default Nav;
